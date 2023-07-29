64f752686a5a35944905b937993e109cfd0c7fcf
Naimas <Naimas@DESKTOP-68Q4MHG>
2023-07-29 18:45:30 +0300

    Completed

---
+++ Firm.cs
@@ -0,0 +1,85 @@
+﻿
+namespace LinQ
+{
+    public class Firma
+    {
+        public string Name { get; set; }
+        public DateTime FoundationDate { get; set; }
+        public string BusinessProfile { get; set; }
+        public string DirectorName { get; set; }
+        public int EmployeeCount { get; set; }
+        public string Address { get; set; }
+        public Firma()
+        {
+            Name = string.Empty;
+            FoundationDate = DateTime.Now;
+            BusinessProfile = string.Empty;
+            DirectorName = string.Empty;
+            EmployeeCount = 0;
+            Address = string.Empty;
+        }
+
+        public Firma(string name, DateTime foundationDate, string businessProfile, string directorName, int employeeCount, string address)
+        {
+            Name = name;
+            FoundationDate = foundationDate;
+            BusinessProfile = businessProfile;
+            DirectorName = directorName;
+            EmployeeCount = employeeCount;
+            Address = address;
+        }
+        public static List<Firma> GetAllFirms(List<Firma> firms) => firms;
+        public static List<Firma> GetFirmsWithNameContains(List<Firma> firms, string keyword)
+        {
+            return firms.Where(f => f.Name.Contains(keyword)).ToList();
+        }
+        public static List<Firma> GetFirmsByBusinessProfile(List<Firma> firms, string businessProfile)
+        {
+            return firms.Where(f => f.BusinessProfile == businessProfile).ToList();
+        }
+        public static List<Firma> GetFirmsByBusinessProfile(List<Firma> firms, string businessProfile1, string businessProfile2)
+        {
+            return firms.Where(f => f.BusinessProfile == businessProfile1 || f.BusinessProfile == businessProfile2).ToList();
+        }
+        public static List<Firma> GetFirmsByEmployeeCountGreaterThan(List<Firma> firms, int employeeCount)
+        {
+            return firms.Where(f => f.EmployeeCount > employeeCount).ToList();
+        }
+        public static List<Firma> GetFirmsByEmployeeCountInRange(List<Firma> firms, int minEmployeeCount, int maxEmployeeCount)
+        {
+            return firms.Where(f => f.EmployeeCount >= minEmployeeCount && f.EmployeeCount <= maxEmployeeCount).ToList();
+        }
+        public static List<Firma> GetFirmsByCity(List<Firma> firms, string city)
+        {
+            return firms.Where(f => f.Address.Contains(city)).ToList();
+        }
+        public static List<Firma> GetFirmsByDirectorLastName(List<Firma> firms, string directorLastName)
+        {
+            return firms.Where(f => f.DirectorName.EndsWith(directorLastName)).ToList();
+        }
+        public static List<Firma> GetFirmsFoundedBefore(List<Firma> firms, DateTime date)
+        {
+            return firms.Where(f => f.FoundationDate < date).ToList();
+        }
+        public static List<Firma> GetFirmsFoundedBefore(List<Firma> firms, int nDays)
+        {
+            return GetFirmsFoundedBefore(firms, DateTime.Now.AddDays(-nDays));
+        }
+        public static List<Firma> GetFirmsByDirectorLastNameAndNameContains(List<Firma> firms, string directorLastName, string keyword)
+        {
+            return firms.Where(f => f.DirectorName.EndsWith(directorLastName) && f.Name.Contains(keyword)).ToList();
+        }
+        public static void PrintFirms(List<Firma> firms)
+        {
+            foreach (var firma in firms)
+            {
+                Console.WriteLine($"Назва: {firma.Name}");
+                Console.WriteLine($"Дата заснування: {firma.FoundationDate:dd.MM.yyyy}");
+                Console.WriteLine($"Профіль бізнесу: {firma.BusinessProfile}");
+                Console.WriteLine($"Директор: {firma.DirectorName}");
+                Console.WriteLine($"Кількість працівників: {firma.EmployeeCount}");
+                Console.WriteLine($"Адреса: {firma.Address}\n");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,52 @@
-﻿namespace LinQ
+﻿using System.Text;
+
+namespace LinQ
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            Firma firma = new Firma();
+            List<Firma> firms = new List<Firma>
+            {
+                new Firma("FoodCorp", new DateTime(1990, 5, 15), "Харчова промисловість", "John Smith", 200, "London"),
+                new Firma("IT Solutions", new DateTime(2005, 8, 20), "IT", "Jane Doe", 150, "New York"),
+                new Firma("Marketing Experts", new DateTime(2010, 3, 10), "Маркетинг", "Michael White", 80, "London"),
+                new Firma("White House", new DateTime(2018, 12, 5), "Інше", "Peter Black", 500, "Washington, D.C."),
+            };
+            Console.WriteLine("Всі фірми:");
+            Firma.PrintFirms(firms);
+
+            Console.WriteLine("\n Фірми, які мають у назві слово \"Food\":");
+            Firma.PrintFirms(Firma.GetFirmsWithNameContains(firms, "Food"));
+
+            Console.WriteLine("\n Фірми, які працюють у галузі маркетингу:");
+            Firma.PrintFirms(Firma.GetFirmsByBusinessProfile(firms, "Маркетинг"));
+
+            Console.WriteLine("\n Фірми, які працюють у галузі маркетингу або IT:");
+            Firma.PrintFirms(Firma.GetFirmsByBusinessProfile(firms, "Маркетинг", "IT"));
+
+            Console.WriteLine("\n Фірми з кількістю працівників більшою, ніж 100:");
+            Firma.PrintFirms(Firma.GetFirmsByEmployeeCountGreaterThan(firms, 100));
+
+            Console.WriteLine("\n Фірми з кількістю працівників у діапазоні від 100 до 300:");
+            Firma.PrintFirms(Firma.GetFirmsByEmployeeCountInRange(firms, 100, 300));
+
+            Console.WriteLine("\n Фірми, які знаходяться в Лондоні:");
+            Firma.PrintFirms(Firma.GetFirmsByCity(firms, "London"));
+
+            Console.WriteLine("\n Фірми, в яких прізвище директора White:");
+            Firma.PrintFirms(Firma.GetFirmsByDirectorLastName(firms, "White"));
+
+            Console.WriteLine("\n Фірми, які засновані більше двох років тому:");
+            Firma.PrintFirms(Firma.GetFirmsFoundedBefore(firms, DateTime.Now.AddYears(-2)));
+
+            Console.WriteLine("\n Фірми з дня заснування яких минуло 123 дні:");
+            Firma.PrintFirms(Firma.GetFirmsFoundedBefore(firms, DateTime.Now));
+
+            Console.WriteLine("\n Фірми, в яких прізвище директора Black і мають у назві фірми слово \"White\":");
+            Firma.PrintFirms(Firma.GetFirmsByDirectorLastNameAndNameContains(firms, "Black", "White"));
         }
     }
 }
